7c42d85d08c5e2473befea8d1bf2eea3c8d1058f
Genildo <genildocs@gmail.com>
2022-09-21 05:11:02 -0300

    terceiro commit

---
+++ Exercicios/cotacaomoeda.py
@@ -0,0 +1,6 @@
+import requests
+
+cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+cotacoes = cotacoes.json()
+cotacao_dolar = cotacoes['USDBRL']['bid']
+print(f'Valor atual do dolar: {cotacao_dolar}')
